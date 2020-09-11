f0acd71a06193a48cdb8481230db55790ddb7223
PedroZ <pedrozenha12@gmail.com>
2020-09-11 10:15:44 +0100

    Table FC

---
+++ src/pages/clients-list/index.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import MaterialTable, { Column } from 'material-table';
+
+interface Row {
+  name: string;
+  dob: string;
+  email: string;
+  phone: string;
+}
+
+const Table: FC = () => {
+  return <></>;
+};
