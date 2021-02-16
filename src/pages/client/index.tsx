239a44e6d04c4aa20372cccd5007be19f404834f
Pedro Zenha <pedrozenha12@gmail.com>
2021-02-16 20:23:35 +0000

    client page card + accordion of observations

--- src/pages/client/index.tsx
+++ src/pages/client/index.tsx
@@ -5,37 +5,63 @@ import MainLayout from '../../layout/main-layout'
 
 import { Card, CardContent } from '@material-ui/core'
 import PersonIcon from '@material-ui/icons/Person';
-import EmailIcon from '@material-ui/icons/Email';
-import EventRoundedIcon from '@material-ui/icons/EventRounded';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
-import PhoneIphoneRoundedIcon from '@material-ui/icons/PhoneIphoneRounded';
+import EmailTwoToneIcon from '@material-ui/icons/EmailTwoTone';
+import EventTwoToneIcon from '@material-ui/icons/EventTwoTone';
+import LocationOnTwoToneIcon from '@material-ui/icons/LocationOnTwoTone';
+import PhoneIphoneTwoToneIcon from '@material-ui/icons/PhoneIphoneTwoTone';
+
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import { ISearchClientsQueryResult } from '../../types'
 import getClient from '../../graphql/queries/get-client'
 import './client-info.scss'
 
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+    },
+    heading: {
+      fontSize: theme.typography.pxToRem(16),
+      flexBasis: '33.33%',
+      flexShrink: 0,
+    },
+    secondaryHeading: {
+      fontSize: theme.typography.pxToRem(15),
+      color: theme.palette.text.secondary,
+    },
+  }),
+);
+
 
 const ClientView: FC = () => {
+  const classes = useStyles();
+
     const [client, setClient] = useState<ISearchClientsQueryResult | null>(null)
     const params = useParams<{id: string}>()
     const _id = params?.id
     const info = [
       {
-        icon: <EventRoundedIcon />,
+        icon: <EventTwoToneIcon fontSize='large' color='primary' />,
         info: `${moment(client?.dob).format('DD/MM/YYYY')} - ${moment().diff(client?.dob, 'years')} anos`
       },
       {
-        icon: <EmailIcon />,
+        icon: <EmailTwoToneIcon fontSize='large' color='primary'/>,
         info: client?.email || ''
       },
       {
-        icon: <PhoneIphoneRoundedIcon />,
+        icon: <PhoneIphoneTwoToneIcon fontSize='large' color='primary' />,
         info: client?.phone || ''
       },
       {
-        icon: <LocationOnIcon/>,
-        info: `${client?.address.street}, ${client?.address.city}  ${client?.address.zipcode}`
+        icon: <LocationOnTwoToneIcon fontSize='large' color='primary' />,
+        info: `${client?.address?.street || ''}, ${client?.address?.city || ''}  ${client?.address?.zipcode || ''}`
       }
     ]
 
@@ -49,11 +75,14 @@ const ClientView: FC = () => {
     },[])
 
     return(
-        <div>
+      <>
+      <div className="content-grid">
+      <div className="cards-wrapper">
+        <div className="card">
       <Card>
         <CardContent>
           <div className="card-wrapper">
-            <PersonIcon />
+            <h2>{client?.name}</h2>
             <div className="info-wrapper-column">
               {info.map((i) => (
                 i.info && (
@@ -66,6 +95,30 @@ const ClientView: FC = () => {
         </CardContent>
       </Card>
         </div>
+
+    
+      </div>            
+
+        <div className="accordion">
+          {client?.observations.map( obs => (
+            <>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                id={obs._id}
+              >
+                <Typography className={classes.heading}>{moment(obs.created_at).format('DD/MM/YYYY HH:mm')}</Typography>
+                <Typography className={classes.secondaryHeading}>{`${obs.description.toString().slice(0,40)}...`}</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <span>{obs.description}</span>
+              </AccordionDetails>
+            </Accordion>
+            </>
+          ))}
+        </div>
+        </div>
+        </>
     )
 }
 
