cdbb668c7e0b811c0f2a8853aa845e0eeb56bf79
Pedro Zenha <pedrozenha12@gmail.com>
2021-03-13 00:01:25 +0000

    add second input field

--- src/pages/auth/sign-in/reset-password-input.tsx
+++ src/pages/auth/sign-in/reset-password-input.tsx
@@ -3,19 +3,35 @@ import { TextField, Dialog, DialogContent, Button, Switch } from '@material-ui/c
 import { Formik } from 'formik'
 import RESET_PASSWORD from '../../../graphql/mutations/reset-password'
 import { client as ApolloClient } from '../../../graphql/client'
+import { useHistory } from 'react-router-dom'
+import { string, object } from 'yup'
 
 import useStyles from './styles-hook'
 
+
+const validationSchema = object().shape({
+	newPassword: string().min(3).required(),
+	repeated: string().min(3).required().test('is-repeated', 'As passwords têm de ser iguais', function(){
+		if(this.parent.newPassword === this.parent.repeated){
+			return true
+		}
+		return this.createError({path: this.path, message: 'As passwords têm de ser iguais'})
+	})
+})
+
 const ResetPassword:FC<{recoveryToken: string}> = ({recoveryToken}) => {
+	const history = useHistory()
 	const classes = useStyles()
 	return(
 		<>
 			<Formik
 				initialValues={{
-					newPassword: ''
+					newPassword: '',
+					repeated: ''
 				}}
+				validationSchema={validationSchema}
 				onSubmit={async (values) => {
-					const res = await ApolloClient.mutate<boolean,{input: {recoveryToken: string, password: string}}>({
+					await ApolloClient.mutate<boolean,{input: {recoveryToken: string, password: string}}>({
 						mutation: RESET_PASSWORD,
 						variables: {
 							input: {
@@ -24,6 +40,7 @@ const ResetPassword:FC<{recoveryToken: string}> = ({recoveryToken}) => {
 							}
 						}
 					})
+					history.push('/home')
 				}}
 			>
 				{({
@@ -42,6 +59,19 @@ const ResetPassword:FC<{recoveryToken: string}> = ({recoveryToken}) => {
 							value={values.newPassword}
 							onChange={handleChange}
 							name="newPassword"
+							type="password"
+							autoFocus
+						/>
+						<TextField
+							variant="outlined"
+							margin="normal"
+							required
+							fullWidth
+							id="repeated"
+							label="Repita a password"
+							value={values.repeated}
+							onChange={handleChange}
+							name="repeated"
 							type="password"
 							autoFocus
 						/>
